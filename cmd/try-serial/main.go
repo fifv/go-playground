4c5b9c8784aaf3d8a58836b6d2741151c5705b02
Fifv <fifvzheyli@gmail.com>
2026-01-19 20:06:23 +0800

    update;

--- cmd/try-serial/main.go
+++ cmd/try-serial/main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"go.bug.st/serial"
+	"go.bug.st/serial/enumerator"
 )
 
 func main() {
@@ -19,12 +20,38 @@ func main() {
 		fmt.Printf("Found port: %v\n", port)
 	}
 
-	port, err := serial.Open("COM21", &serial.Mode{BaudRate: 115200})
-	if err != nil {
-		log.Fatal(err)
+	{
+		ports, err := enumerator.GetDetailedPortsList()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(ports) == 0 {
+			fmt.Println("No serial ports found!")
+			return
+		}
+		for _, port := range ports {
+			fmt.Printf("Found port: %s\n", port.Name)
+			fmt.Printf("   Product: %s\n", port.Product)
+			if port.IsUSB {
+				fmt.Printf("   USB ID     %s:%s\n", port.VID, port.PID)
+				fmt.Printf("   USB serial %s\n", port.SerialNumber)
+			}
+		}
 	}
 
-	n, err := port.Write([]byte("asfdf112fffffffff"))
+	var port serial.Port
+	for {
+		port, err = serial.Open("COM21", &serial.Mode{BaudRate: 115200})
+		if err != nil {
+			log.Println(err)
+		} else {
+			fmt.Println("Connected To COM  ")
+			break
+		}
+		time.Sleep(time.Millisecond * 300)
+	}
+
+	n, err := port.Write([]byte("fasdfa"))
 	if err != nil {
 		log.Fatal(err)
 	}
